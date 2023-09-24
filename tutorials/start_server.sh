ee03b648433596617ac318016df3bfc97dcd2fc8
kenho <kenho811@gmail.com>
2023-09-24 13:53:45 +0800

    update documentation

--- tutorials/start_server.sh
+++ tutorials/start_server.sh
@@ -5,5 +5,7 @@ set -e
 pip install -r ./requirements.txt
 cd ./api_server_one
 
-./app.py # start the HTTP server
+./app.py & # start the HTTP server in background
+
+
 echo 'please check http://localhost:8080/ui'
