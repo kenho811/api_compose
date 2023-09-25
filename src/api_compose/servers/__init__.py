2461c10187802baf1fb2e205d79c238663d4bb5b
kenho <kenho811@gmail.com>
2023-09-25 22:03:18 +0800

    fix swagger UI

--- src/api_compose/servers/__init__.py
+++ src/api_compose/servers/__init__.py
@@ -1,11 +1,14 @@
+from api_compose.servers.prefixes import __API_SERVER_ONE_PREFIX__
+
+
 def setup_api_server_one():
     """Entrypoint for Jupyter server proxy"""
     return {
-        'command': ['python', '-m', 'api_compose.servers.api_server_one.app', '{port}'],
+        'command': ['python', '-m', 'api_compose.servers.api_server_one.app', '{port}', __API_SERVER_ONE_PREFIX__],
         'port': 8080,
+        'absolute_url': True,
         'launcher_entry': {
             'enabled': True,
-            'icon_path': '/tmp/.jupyter/api_server_one.png',
             'title': 'ApiServerOne',
         },
     }
---
+++ src/api_compose/servers/prefixes.py
@@ -0,0 +1,1 @@
+__API_SERVER_ONE_PREFIX__ = 'api_server_one'
