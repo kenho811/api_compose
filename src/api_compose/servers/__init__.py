225ae32387bf601baf6cb38d5629503a4c352ed0
kenho811 <57944769+kenho811@users.noreply.github.com>
2023-09-26 09:04:57 +0800

    Update __init__.py

--- src/api_compose/servers/__init__.py
+++ src/api_compose/servers/__init__.py
@@ -4,7 +4,7 @@ from api_compose.servers.prefixes import __API_SERVER_ONE_PREFIX__, __API_SERVER
 def setup_api_server_one():
     """Entrypoint for Jupyter server proxy"""
     return {
-        'command': ['python', '-m', 'api_compose.servers.api_server_one.app', '{port}', __API_SERVER_ONE_PREFIX__],
+        'command': ['python', '-m', 'api_compose.servers.api_server_one.app', '{port}', '{base_url}' + __API_SERVER_ONE_PREFIX__],
         'port': __API_SERVER_ONE_PORT__,
         'absolute_url': True,
         'launcher_entry': {
